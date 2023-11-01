33e810a928617326ae55f1423e20eaba4c0dee96
DanielDev1984 <60503176+DanielDev1984@users.noreply.github.com>
2023-11-01 09:17:39 +0100

    adapt windowing according to image histogram

--- QtDicomVtk/QtDicomVtk.cpp
+++ QtDicomVtk/QtDicomVtk.cpp
@@ -4,9 +4,12 @@
 #include <vtkFileOutputWindow.h>
 #include <vtkImageActor.h>
 #include <vtkImageHistogram.h>
-
+#include <vtkImageSlice.h>
+#include <vtkImageProperty.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkDICOMImageReader.h>
+
+#include <sstream>
 
 QtDicomVtk::QtDicomVtk(QWidget *parent)
     : QMainWindow(parent),
@@ -31,7 +34,7 @@ QtDicomVtk::QtDicomVtk(QWidget *parent)
 
 
     // setup bg-color
-    m_renderer_bg->SetBackground(1.0, 0.0, 0.0);
+    m_renderer_bg->SetBackground(0.3, 0.3, 0.3);
 
     // set ui-connection
     QObject::connect(ui.pushButton, &QPushButton::clicked, this, &QtDicomVtk::onDrawSphereClicked);
@@ -43,14 +46,14 @@ QtDicomVtk::~QtDicomVtk()
 
 void QtDicomVtk::onDrawSphereClicked()
 {
-    vtkOutputWindow::GetInstance()->DisplayText("test");
+
 
     vtkSmartPointer<vtkDICOMImageReader> imageReader_bG{ vtkSmartPointer<vtkDICOMImageReader>::New() };
     constexpr auto fN_bG{ ".\\data\\vhf29.dcm" };
     imageReader_bG->SetFileName(fN_bG);
     imageReader_bG->Update();
     const auto imageData_bG{ imageReader_bG->GetOutput() };
-    
+
     vtkNew<vtkImageHistogram> histogram;
     histogram->SetInputConnection(imageReader_bG->GetOutputPort());
     histogram->GenerateHistogramImageOn();
@@ -58,11 +61,27 @@ void QtDicomVtk::onDrawSphereClicked()
     histogram->SetHistogramImageScaleToSqrt();
     histogram->AutomaticBinningOn();
     histogram->Update();
+
+    vtkIdType nbins = histogram->GetNumberOfBins();
+    double range[2];
+    const auto level{ histogram->GetBinOrigin() };
+    const auto width{level + (nbins - 1) * histogram->GetBinSpacing()};
+
+    std::stringstream ss;
+    ss << "level: " << level << "width: " << width;
+    const std::string tmp = ss.str();
+    const char* cstr = tmp.c_str();
+    vtkOutputWindow::GetInstance()->DisplayText(cstr);
+    
+    
     const auto histogramData_bG{ histogram->GetOutput() };
 
 
     vtkSmartPointer<vtkImageActor> actor_bG{ vtkSmartPointer<vtkImageActor>::New() };
-    actor_bG->SetInputData(histogramData_bG);
+    
+    actor_bG->SetInputData(imageData_bG);
+    actor_bG->GetProperty()->SetColorWindow(width);
+    actor_bG->GetProperty()->SetColorLevel(level);
     
     m_renderer_bg->AddViewProp(actor_bG);
     m_renderer_bg->ResetCamera();
